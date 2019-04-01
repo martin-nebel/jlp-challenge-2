dc86ca558c66a1d5488a2acc897e2289f067ac6d
Martin Nebel <martin.nebel@johnlewis.com>
2019-04-01 12:32:17 +0100

    1.0.1 Indent code by 4 chars rather than my preferred 2 :-)

--- src/fixPriceLabel.js
+++ src/fixPriceLabel.js
@@ -3,7 +3,7 @@ function fixPriceLabel(priceLabelString) {
   const extractPriceFromPriceString = priceString => priceString.split("£")[1];
 
   const isPriceHigherThanAllPricesAfterIt = (thisPrice, remainingPrices) => {
-    return parseFloat(thisPrice) > Math.max(...remainingPrices);
+      return parseFloat(thisPrice) > Math.max(...remainingPrices);
   }
 
   const isFirstPrice = (price, listOfPrices) => price === listOfPrices[0];
@@ -11,22 +11,22 @@ function fixPriceLabel(priceLabelString) {
   const isLastPrice = (price, listOfPrices) => price === listOfPrices.slice(-1).pop();
 
   const formatPriceString = (price, listOfPrices) => {
-    return isFirstPrice(price, listOfPrices)
-      ? (isOnlyPrice(listOfPrices) ? "Now £" + price : "Was £" + price)
-      : (!isLastPrice(price, listOfPrices) ? " then £" + price : " now £" + price)
+      return isFirstPrice(price, listOfPrices)
+          ? (isOnlyPrice(listOfPrices) ? "Now £" + price : "Was £" + price)
+          : (!isLastPrice(price, listOfPrices) ? " then £" + price : " now £" + price)
   }
 
   return priceLabelString
-    .split(",")
-    .map(priceString => extractPriceFromPriceString(priceString))
-    .filter((price, positionOfPriceInList, listOfPrices) => {
-      return isPriceHigherThanAllPricesAfterIt(price, listOfPrices.slice(positionOfPriceInList + 1));
-    })
-    .map((filteredPrice, positionOfPriceInList, listOfFilteredPrices) => {
-      return formatPriceString(filteredPrice, listOfFilteredPrices);
-    })
-    .join(",")
-    ;
+      .split(",")
+      .map(priceString => extractPriceFromPriceString(priceString))
+      .filter((price, positionOfPriceInList, listOfPrices) => {
+          return isPriceHigherThanAllPricesAfterIt(price, listOfPrices.slice(positionOfPriceInList + 1));
+      })
+      .map((filteredPrice, positionOfPriceInList, listOfFilteredPrices) => {
+          return formatPriceString(filteredPrice, listOfFilteredPrices);
+      })
+      .join(",")
+      ;
 }
 
 module.exports = fixPriceLabel;
